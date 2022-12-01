e9e24ac17480119078c7b0395284802102205d07
smthwsmth <smthwsmth@gmail.com>
2022-12-01 12:54:59 +0500

    New file

--- Experiments/hash_fun.py
+++ Experiments/hash_fun.py
@@ -1,13 +1,39 @@
-def remove_marks(text, marks):
-    for i in marks:
-        text = text.replace(i, '')
-    remove_marks.count += 1
-    return text
+def deliting(mi, group):
+    if isinstance(group, set):
+        group.discard(mi)
+    elif isinstance(group, list):
+        del group[group.index(mi)]
+    elif isinstance(group, tuple):
+        group = list(group)
+        del group[group.index(mi)]
+    return group
 
-remove_marks.__dict__.setdefault('count', 0)
 
-text = 'Hi! Will we go together?'
+def sort_priority(values, group):
+    def h():
+        ans = []
+        nonlocal values, group
+        while True:
+            if len(group) == 0:
+                break
+            else:
+                mi = min(group)
+                if mi in values:
+                    id = values.index(mi)
+                    ans.append(values[id])
+                    del values[id]
+                    group = deliting(mi, group)
+                else:
+                    group = deliting(mi, group)
+        
+        ans.extend(sorted(values))
+        values[:] = ans
+    h()
+            
+numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+group = {5, 7, 2, 3}
+sort_priority(numbers, group)
 
-print(remove_marks.count)
-print(remove_marks(text, '!?'))
-print(remove_marks.count)
+print(numbers)
+
+
---
+++ Experiments/some_exp.py
@@ -0,0 +1,6 @@
+
+
+def print_hello(num1: int, num2: int, num3: int) -> float:
+    return (num1 + num2 + num3) / 3
+
+print(print_hello.__annotations__)
